d61b78e943978e7b8ca5e7f4d138321cb5095a9f
Lukas Brackmann <157488023+ProgrammingGoat@users.noreply.github.com>
2024-07-18 15:39:49 +0200

    formatting

--- tests/test_app.py
+++ tests/test_app.py
@@ -18,11 +18,14 @@ def test_ratio_10_50():
 def test_coffee_from_water_1_1():
     assert calcs.calculate_coffee_from_water(1, 1) == 1
 
+
 def test_coffee_from_water_500_0_5():
     assert calcs.calculate_coffee_from_water(500, 0.5) == 250
 
+
 def test_water_from_coffee_1_1():
     assert calcs.calculate_water_from_coffee(1, 1) == 1
+
 
 def test_water_from_coffee_500_0_5():
     assert calcs.calculate_water_from_coffee(500, 0.5) == 1000
